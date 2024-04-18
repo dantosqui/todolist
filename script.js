b93ec3bba406f30523f0ae6f07bc6773da357011
zareke <34665479+zareke@users.noreply.github.com>
2024-04-17 23:57:39 -0300

    commit3

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ var listaProyectos = []
 var listaToDos = []
 var checkId = 0
 var labels = []
-
+var tachados=[]
 
 function AgregarElemento(taskDescription,dateOfVencimiento) {
+    if(listaProyectos.length == 0){
+        alert("Necesitas crear un proyecto para crear una tarea")
+        return;
+    }else if(listaProyectos[document.getElementById("dD").value].todos.some(x => x.nombre == taskDescription) && listaProyectos[document.getElementById("dD").value].todos.some(x => x.dateOfVencimiento == dateOfVencimiento)){
+        alert("Una tarea con el mismo nombre y fecha de vencimiento ya existe")
+        return;
+    }//decime cuando pueda probar el codigo RIGHT HERE RIGHT NOW ok listo
     if(dateOfVencimiento == '' || taskDescription==''){
         alert("Debe insertar una fecha de vencimiento y nombre")
         return
@@ -13,17 +20,17 @@ function AgregarElemento(taskDescription,dateOfVencimiento) {
             
         
         iD=document.getElementById("dD").value
-        const fechaDeHoy = new Date().toLocaleString("es-ES")
+        const fechaDeHoy = new Date().toLocaleString("en-ES") //
         listaProyectos[iD].todos.push({
             "nombre": taskDescription,
             "fecha": fechaDeHoy,
-            "tachado": false,
             "fechaTachado": null,
             "cross": null,
+            "tachado":true,
             "dateOfVencimiento" : dateOfVencimiento,
             "checkbox":0
         })
-        
+        tachados.push(false)
         
         MostrarDesdeCero(listaProyectos[iD].todos)
         
@@ -35,11 +42,11 @@ function MostrarDesdeCero(listaTasks){ //hacer que muestre el nombre y descripci
     labels =[]
     checkId=0
     console.log("typof: ", typeof listaTasks)
-    
+    lista=listaTasks
     if (typeof listaTasks != "object"){
         listaTasks=listaProyectos[document.getElementById("dD").value].todos
     }
-    
+    console.log("hola")
     document.getElementById("listaActual").innerHTML=""
     listaTasks.forEach(i => {
         
@@ -52,10 +59,8 @@ function MostrarDesdeCero(listaTasks){ //hacer que muestre el nombre y descripci
         checkbox.id = checkId
         i.checkbox = checkId
         
-        checkbox.setAttribute("onclick", `Tachar(${i.checkbox},${listaTasks})`); //funciones de la checkbox
-        checkbox.setAttribute("onmouseover", `MostrarFecha(${i.checkbox},${listaTasks})`);
-        checkbox.setAttribute("onmouseout", `SacarFecha(${i.checkbox},${listaTasks})`);
-
+        checkbox.setAttribute("onclick", `Tachar(${i.checkbox},${JSON.stringify(i)},${JSON.stringify(listaProyectos[document.getElementById("dD").value].todos)})`);
+        
         var label = task.appendChild(document.createElement("label")) //le creamos una label
         label.innerHTML = i.nombre
         labels.push(label)
@@ -65,9 +70,9 @@ function MostrarDesdeCero(listaTasks){ //hacer que muestre el nombre y descripci
         cross.innerHTML = "x"
         cross.style.color = "red"
         i.cross = cross
-        cross.setAttribute("onclick", `BorrarTask(${checkbox.id})`);
-        
+        cross.setAttribute("onclick", `BorrarTask(${checkbox.id},${JSON.stringify(i.nombre)},${JSON.stringify(i.dateOfVencimiento)})`);
         checkId++ //cada task tiene un checkid, se reinicia cuando mostramos de nuevo
+        
     });
 
 }
@@ -94,8 +99,17 @@ function TareaMasRapidaEnRealizarse(idProyecto) { //actualizar esto
     var fechaRapida = new Date()   
     var nombreTarea
     var proyecto
-
-
+    var menorFecha = 0
+    var fechaDate
+    var fechaVenc
+    var diaDate
+    var mesDate
+    var anioDate
+    var diaVenc
+    var mesVenc
+    var anioVenc
+    var ddd = 0
+    var j = 1
     proyecto = listaProyectos[idProyecto]
     console.log("nombre proyecto: " + proyecto.nombre)
     console.log("drfgvsg")
@@ -116,78 +130,119 @@ function TareaMasRapidaEnRealizarse(idProyecto) { //actualizar esto
             //         console.log("Fecha rapida: " + fechaRapida + nombreTarea)
             //     }
             // }
+            console.log("ppppppppp" + proyecto.todos[i].fecha)
+            // fechaDate = new Date(proyecto.todos[i].fecha)
+            // diaDate = fechaDate.getDate();
+            // mesDate = fechaDate.getMonth() + 1
+            // anioDate = fechaDate.getFullYear()
 
-            if (proyecto.todos[i] != null && document.getElementById(i).checked) {
-                // if (proyecto.todos[i].fecha == null || proyecto.todos[i].fecha > listaToDos[i].fechaTachado) {
-                //     fechaRapida = proyecto.todos[i].fechaTachado
-                //     nombreTarea = proyecto.todos[i].nombre
-                //     console.log("Fecha rapida: " + fechaRapida + nombreTarea)
-                // }
-                console.log("cccccc")
-                console.log("fecha vencimiento " + proyecto.todos[i].dateOfVencimiento)
-                console.log("sdsdj")
-                console.log("fecha tachado " + proyecto.todos[i].fechaTachado)
-                if(proyecto.todos[i].dateOfVencimiento.toLocaleString("es-ES") - proyecto.todos[i].fechaTachado.toLocaleString("es-ES") < fechaRapida.toLocaleString("es-ES")){
-                    fechaRapida = proyecto.todos[i].dateOfVencimiento.toLocaleString("es-ES") - proyecto.todos[i].fechaTachado.toLocaleString("es-ES")
+            fechaVenc = new Date(proyecto.todos[i].dateOfVencimiento)
+            diaVenc = fechaVenc.getDate();
+            mesVenc = fechaVenc.getMonth() +1
+            anioVenc = fechaVenc.getFullYear()
+
+            fechaVenc2 = new Date(proyecto.todos[j].dateOfVencimiento)
+            diaVenc2 = fechaVenc2.getDate();
+            mesVenc2 = fechaVenc2.getMonth() +1
+            anioVenc2 = fechaVenc2.getFullYear()
+
+            
+            if(anioVenc > anioVenc2){
+                menorFecha = proyecto.todos[j].dateOfVencimiento
+                
+            }else if(anioVenc < anioVenc2){
+                menorFecha = proyecto.todos[i].dateOfVencimiento
+                nombreTarea = proyecto.todos[i].nombre
+            }else{
+            if(mesVenc > mesVenc2){
+                menorFecha = proyecto.todos[j].dateOfVencimiento
+                nombreTarea = proyecto.todos[j].nombre
+            }else if(mesVenc < mesVenc2){
+                menorFecha = proyecto.todos[i].dateOfVencimiento
+                nombreTarea = proyecto.todos[i].nombre
+            }else{
+                if(diaVenc > diaVenc2){
+                    menorFecha = proyecto.todos[j].dateOfVencimiento
+                    nombreTarea = proyecto.todos[j].nombre
+                }else{
+                    menorFecha = proyecto.todos[i].dateOfVencimiento
                     nombreTarea = proyecto.todos[i].nombre
-                    console.log("Fecha rapida: " + fechaRapida + nombreTarea)
                 }
             }
         }
-        console.log(fechaRapida)
 
-        if (fechaRapida != null) {
-            text.innerHTML = `La tarea "${nombreTarea}" fue la que mas rapido se realizó, el dia ${fechaRapida.toLocaleString("es-ES")}`
+            console.log(typeof proyecto.todos[i].fecha + " typeof de la fecha");
+            console.log("fecha en numero: " + menorFecha)
+
+
+            
+
+            // if(menorFecha < Number(proyecto.todos[i].dateOfVencimiento) - Number(proyecto.todos[i].fecha))
+            // {
+            //     console.log("fsjsfljfosjfsklfse")
+            //     menorFecha = Number(proyecto.todos[i].dateOfVencimiento) - Number(proyecto.todos[i].fecha)
+            //     nombreTarea = proyecto.todos[i].nombre
+            //     ddd = i 
+            // }
+            console.log(menorFecha)
+
+            // if (proyecto.todos[i] != null && document.getElementById(i).checked) {
+            //     // if (proyecto.todos[i].fecha == null || proyecto.todos[i].fecha > listaToDos[i].fechaTachado) {
+            //     //     fechaRapida = proyecto.todos[i].fechaTachado
+            //     //     nombreTarea = proyecto.todos[i].nombre
+            //     //     console.log("Fecha rapida: " + fechaRapida + nombreTarea)
+            //     // }
+            //     console.log("cccccc")
+            //     console.log("fecha vencimiento " + proyecto.todos[i].dateOfVencimiento)
+            //     console.log("sdsdj")
+            //     console.log("fecha tachado " + proyecto.todos[i].fechaTachado)
+            //     if(proyecto.todos[i].dateOfVencimiento.toLocaleString("es-ES") - proyecto.todos[i].fechaTachado.toLocaleString("es-ES") < fechaRapida.toLocaleString("es-ES")){
+            //         fechaRapida = proyecto.todos[i].dateOfVencimiento.toLocaleString("es-ES") - proyecto.todos[i].fechaTachado.toLocaleString("es-ES")
+            //         nombreTarea = proyecto.todos[i].nombre
+            //         console.log("Fecha rapida: " + fechaRapida + nombreTarea)
+            //     }
+            // }
+            j--;
+        }
+        console.log("la fecha mas rapida en completarse " + menorFecha)
+
+        if (menorFecha != null) {
+            text.innerHTML = `La tarea "${nombreTarea}" fue la que mas rapido se realizó, ${menorFecha}`
         } else {
             text.innerHTML = `No hay ninguna tarea tachada`
         }
-
     } else {
         text.innerHTML = `No hay ninguna tarea`
     }
 }
 
-function BorrarTask(idCheck) {
-    proy = listlistaProyectos[document.getElementById("dD").value].todos
+function BorrarTask(idCheck, nombreNow, fVNow) {
     console.log(idCheck)
     document.getElementById(`li_${idCheck}`).remove()
-    delete proy[idCheck]
+    listaProyectos[document.getElementById("dD").value].todos = listaProyectos[document.getElementById("dD").value].todos.filter(x=>x.nombre!=nombreNow && x.dateOfVencimiento!=fVNow)
+    console.log(listaProyectos[document.getElementById("dD").value].todos)
 }
 
-function Tachar(idCheck,list) {
-    proy = list
-    proy[idCheck].fechaTachado = new Date().toLocaleString("es-ES")
-    if (document.getElementById(idCheck).checked) {
-        labels[idCheck].innerHTML = proy[idCheck].nombre.strike() + ` - ${proy[idCheck].fechaTachado}`
+function Tachar(idCheck) {
+    proy = lista[idCheck]
+    console.log(proy)
+    proy.fechaTachado = new Date().toLocaleString("en-EN")
+    proy.tachado = !proy.tachado
+    if (proy.tachado) {
+        labels[idCheck].innerHTML = proy.nombre + ` - Vencimiento: ${proy.dateOfVencimiento} - Tachado: ${proy.fechaTachado}`
+        console.log(proy.tachado)
+        document.getElementById(idCheck).checked = true
+        console.log(proy.tachado)
 
-    } else {
-        labels[idCheck].innerHTML = proy[idCheck].nombre + ` - ${proy[idCheck].fecha}`
-    }
-    
+    } 
+    else {
+        labels[idCheck].innerHTML = proy.nombre + ` - Vencimiento: ${proy.dateOfVencimiento}`
+        document.getElementById(idCheck).checked = false
+    }   
 }
-
-function MostrarFecha(idCheck,list) {
-    proy = list
-    proy[idCheck].cross.style.visibility = "hidden"
-    if (labels[idCheck].firstElementChild)
-        labels[idCheck].innerHTML += ` - ${proy[idCheck].fechaTachado}`
-    else
-        labels[idCheck].innerHTML += ` - ${proy[idCheck].fecha}`
-}
-
-function SacarFecha(idCheck,list) {
-    proy = listaProyectos[document.getElementById("dD").value].todos
-    proy[idCheck].cross.style.visibility = "visible"
-    if (labels[idCheck].firstElementChild)
-        labels[idCheck].innerHTML = proy[idCheck].nombre.strike()
-    else
-        labels[idCheck].innerHTML = proy[idCheck].nombre
-}
-
 //NUEVO TP FUNCIONES A PARTIR DE ACA: 
 indexProyectos = 0
 function AgregarProyecto(nombre, descripcion) {
-    
     
     listaProyectos.push({
         "nombre":nombre,
@@ -209,20 +264,21 @@ function MostrarProyectos(nombre,indexProyectos){
     nuevoDrop.setAttribute('value', indexProyectos)
     nuevoDrop.innerHTML = nombre
     nuevoDrop.selected = true
-
 }
 
 function Buscar(fe){
-
-   listasConLaFechaDeVencimientoCondicionada =  listaProyectos[document.getElementById("dD").value].todos.filter(function (x) {
-if(x.dateOfVencimiento == fe) return x
-    })
+    if(listaProyectos.length==0) {alert("No hay ningun proyecto");return}
+if(listaProyectos[document.getElementById("dD").value].todos.length==0) {alert("No hay tareas para buscar");return}
+    listasConLaFechaDeVencimientoCondicionada =  listaProyectos[document.getElementById("dD").value].todos.filter(function (x) { if(x.dateOfVencimiento == fe) {return x}})
+    console.log("h") 
+    for(var i=0; i< listasConLaFechaDeVencimientoCondicionada.length; i++){
+        console.log(listasConLaFechaDeVencimientoCondicionada[i].nombre)
+    }
     console.log(listasConLaFechaDeVencimientoCondicionada)
     MostrarDesdeCero(listasConLaFechaDeVencimientoCondicionada)
-    
 }
 function DesBuscar(){
-
-     MostrarDesdeCero(listaProyectos[document.getElementById("dD").value].todos)
+    if(listaProyectos.length==0) {alert("No hay ningun proyecto");return}
+    MostrarDesdeCero(listaProyectos[document.getElementById("dD").value].todos)
      
  }
